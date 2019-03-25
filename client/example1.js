94294195a35b3f35b0af1c71081efb777ce3be0b
Andrew Clark (RIT Student) <alc7546@ad.rit.edu>
2019-03-25 14:50:32 -0400

    Beginning React

---
+++ client/example1.js
@@ -0,0 +1,16 @@
+const HelloWorld = () => {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+};
+
+const init = () => {
+    ReactDOM.render(
+        <HelloWorld />,
+        document.getElementById('app')
+    );
+};
+
+window.onload = init;
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -10,6 +10,15 @@ const example3 = (req, res) => {
   res.render('example3');
 };
 
+const getSongs = (req,res) => {
+  res.json([
+    { artist: "c418", title: "The entire soundtrack"},
+    { artist: "Yeet", title: "Yeet's soundtrack"},
+    { artist: "Yeet v2", title: "v2 soundtrack"},
+  ]);
+};
+
 module.exports.example1 = example1;
 module.exports.example2 = example2;
 module.exports.example3 = example3;
+module.exports.getSongs = getSongs;
